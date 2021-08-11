4a38bc7ade7e8c79855c0a9cb2890d717cdb276d
yvbondarenko <yv.bondarenko@gmail.com>
2021-08-12 01:00:54 +0300

    Update kill-all.sh

--- kill-all.sh
+++ kill-all.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-kill -9 `ps aux|grep 'generateIntervalMs' |grep -v grep | awk '$1 {print $2}'`
+count=$(ps aux|grep 'generateIntervalMs' |grep -v grep | awk '$1 {print $2}' | wc -l)
+if (( $count > 0 )); then
+    kill -9 `ps aux|grep 'generateIntervalMs' |grep -v grep | awk '$1 {print $2}'`
+else
+    echo "No process"
+fi
