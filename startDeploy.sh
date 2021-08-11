7064aba1825bb224bfb8b145701265502c9be7cd
yvbondarenko <yv.bondarenko@gmail.com>
2021-08-11 23:45:16 +0300

    Modify start script

--- startDeploy.sh
+++ startDeploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 kill -9 `ps aux|grep 'apm-poc-java.jar' | awk '$1 {print $2}'`
 input="./deployConfig.conf"
+i=1
 while read line
 do
+
 if [[ -z $line ]]
     then
          exit
     fi
-  command="nohup java -cp apm-poc-java.jar yb.Main \"$line\" &"
+  cp apm-poc-java.jar $i.jar
+  command="nohup java -cp $i.jar yb.Main \"$line\" &"
   eval r=$command
+  let "i++"
 done < $input
