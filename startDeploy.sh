f414a18e86d382dcc05e6cd12858eedf6df1ce98
yvbondarenko <yv.bondarenko@gmail.com>
2021-08-12 01:10:23 +0300

    Update startDeploy.sh

--- startDeploy.sh
+++ startDeploy.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 JENKINS_NODE_COOKIE=dontKillMe
-echo "Start"
 input="./deployConfig.conf"
-echo "1"
 i=1
 while read line
 do
-echo "2"
 if [[ -z $line ]]
     then
         echo "3"
          exit
     fi
-  echo "4"
   cp apm-poc-java.jar $i.jar
-  echo "5"
   command="nohup java -cp $i.jar yb.Main \"$line\" &"
-  echo "6"
   echo $command
-  echo "5"
-  eval $command
-  echo "8"
+  eval r=$command
   let "i++"
-  echo "9"
 done < $input
-echo "10"
