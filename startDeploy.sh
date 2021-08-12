b17f2c984c6f7c9de5ef496862bae38a6417a7a9
yvbondarenko <yv.bondarenko@gmail.com>
2021-08-12 10:52:20 +0300

    java param

--- startDeploy.sh
+++ startDeploy.sh
@@ -6,11 +6,11 @@ while read line
 do
 if [[ -z $line ]]
     then
-        echo "3"
          exit
     fi
   cp apm-poc-java.jar $i.jar
-  command="nohup java -cp $i.jar yb.Main \"$line\" &"
+  dParam=$(echo $line | cut -f1 -d";")
+  command="nohup java -D$dParam -cp $i.jar yb.Main \"$line\" &"
   eval r=$command
   let "i++"
 done < $input
